37861e9f13f674ea5ad3d44c8979ecc7af22b616
mmmhasan-react <2md.mahmudul.hasan@gmail.com>
2024-11-20 06:56:07 +0600

    project setup

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import mongoose from "mongoose";
-import config from "./app/config";
-import app from "./app";
-
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 async function main() {
   try {
     await mongoose.connect(config.db_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${port}`);
+      console.log(`Example app listening on port 5000 ${config.port}`);
     });
   } catch (err) {
     console.log(err);
